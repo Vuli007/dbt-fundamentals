edd64dc5921337b21ace85b19f8503ec0c386b78
vuli@bulaoutsourcing.com <vuli@bulaoutsourcing.com>
2022-04-13 07:00:41 +0000

    refactor sqls

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,22 @@
+With
+
+source as(
+
+    Select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+customers as(
+
+    select 
+       
+        id as customer_id,
+        last_name as surname,
+        first_name as givenname,
+        first_name || ' ' || last_name as full_name
+        
+      from source
+
+)
+
+Select * from customers
---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,21 @@
+ with 
+
+ source as (
+
+     Select * from {{ source('stripe', 'payment') }}
+
+ ),
+
+ payments as (
+
+    Select 
+        id as payment_id,
+        orderid as order_id,
+        status as payment_status,
+        round(amount/100.0,2) as payment_amount
+
+    from source
+
+ )
+
+ Select * from payments
