a4d4226e22fe09dc20fd46d980ece439ec6dc100
vuli@bulaoutsourcing.com <vuli@bulaoutsourcing.com>
2022-05-11 05:14:32 +0000

    Updating model file structure into facts and dims

--- macros/utc_to_ist.sql
+++ macros/utc_to_ist.sql
@@ -1,7 +1,6 @@
 {% macro convert_utc_to_ist(column_name) -%}
 
 --{{ column_name }} at time zone 'India Standard Time' 
---CONVERT(DATETIME, {{ column_name }} AT TIME ZONE 'India Standard Time')
 
 dateadd(minutes, 330,{{ column_name }})
 
