7cffd2e12392bfc14b47de9ff81ea4d478477642
vuli@bulaoutsourcing.com <vuli@bulaoutsourcing.com>
2022-05-11 06:45:02 +0000

    minor changes as clarified from Rahul

--- models/india_warehouse_models/marts/facts/fct_activities/fct_activities.sql
+++ models/india_warehouse_models/marts/facts/fct_activities/fct_activities.sql
@@ -11,7 +11,7 @@ with activities as(
     
     from {{ ref('stg_activities')}}
     where [activity_event] in (21,22,201,206,207,213)
-    and created_on_IST > '2021-08-31 23:59:59'              -- restricted time frame which needs to be clarified
+
 )
 
 Select * from activities
