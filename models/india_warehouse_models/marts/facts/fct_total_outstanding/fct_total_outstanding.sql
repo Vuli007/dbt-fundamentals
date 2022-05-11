495e8c9194375821b3564a358dbefd1c24fa0563
vuli@bulaoutsourcing.com <vuli@bulaoutsourcing.com>
2022-05-11 06:59:25 +0000

    Get most recent tos for each pinstid

--- models/india_warehouse_models/marts/facts/fct_total_outstanding/fct_total_outstanding.sql
+++ models/india_warehouse_models/marts/facts/fct_total_outstanding/fct_total_outstanding.sql
@@ -5,6 +5,17 @@
 with total_outstanding as(
 
     select * from {{ ref('stg_total_outstanding')}}
+),
+
+most_recent as (
+
+    select *
+    from total_outstanding as tos_1
+    where calculated_date = 
+
+        (	select max(calculated_date) as calculated_date                                                -- selecting the most recent pinstid
+            from total_outstanding as tos_2 where tos_1.pinstid = tos_2.pinstid 
+        )
 )
 
-Select * from total_outstanding
+Select * from most_recent
