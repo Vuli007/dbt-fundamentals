5fcd5e3e5fb82ced26def8bd8457fc5ab942e47c
vuli@bulaoutsourcing.com <vuli@bulaoutsourcing.com>
2022-05-17 05:55:33 +0000

    Updating business rules from Rahul

--- models/india_warehouse_models/marts/dims/dim_borrower/dim_borrower.sql
+++ models/india_warehouse_models/marts/dims/dim_borrower/dim_borrower.sql
@@ -5,16 +5,16 @@
 with borrower as(
 
     select *,
-        case when trust_name = 'Arcil-Retail Loan Portfolio-017-B-Trust (Bank_of_India)' then 'Arcil_BOI'  -- derive field portfolio from field 'trust_name'
+        case when trust_name = 'Arcil-Retail Loan portfolio-017-B-Trust (Bank_of_India)' then 'Arcil_BOI'  -- derive field portfolio from field 'trust_name'
         when trust_name = 'Arcil Bajaj Finance - Consumer Loans' then 'Arcil Bajaj-Consumer'
         when trust_name = 'Arcil Bajaj Finance - Personal Loans' then 'Arcil Bajaj-Personal'
         when trust_name = 'Arcil Bajaj Finance - SME Loans' then 'Arcil Bajaj-SME'
         when trust_name like '%Arcil%' AND trust_name like '%LOT 3%' then 'Arcil(Secured Cases)'
         when trust_name like '%South Indian Bank%' then 'South Indian Bank' 
         when trust_name like '%arcil%' then 'Arcil'
-        else trust_name end as Portfolio    --check caps
+        else trust_name end as portfolio   
 
-        --check for duplicates for pinstid - throw a flag if any
+        --check for duplicates for pinstid - throw a flag if any - reconfirm with Rahul
 
 
     from {{ ref('stg_borrower')}}
@@ -22,47 +22,47 @@ with borrower as(
     
 ),
 
-client as (                 -- check like vs contains from excel
+client as (                 
 
     select *,
 
-        case when Portfolio like 'Arcil' then 'Arcil' -- check diff
-        when Portfolio like 'Karur' then 'KVB'
-        when Portfolio like 'Bank Of Maharashtra' then 'Bank Of Maharashtra' -- check diff
-        when Portfolio like 'Omkara-PS 21/2020-21 Trust (Indian Bank - EL)' then 'Omkara-ARC'
-        when Portfolio like 'South Indian Bank' then 'South Indian Bank'
-        when Portfolio like 'City_Union' then 'CUB' -- check diff
-        when Portfolio like 'City_Union' then 'CUB' -- check diff
-        when Portfolio like 'Indian' then 'Indian Bank' 
-        else Portfolio end as client
+        case when portfolio like 'Arcil' then 'Arcil' 
+        when portfolio like 'Karur' then 'KVB'
+        when portfolio like 'Bank Of Maharashtra' then 'Bank Of Maharashtra' 
+        when portfolio like 'Omkara-PS 21/2020-21 Trust (Indian Bank - EL)' then 'Omkara-ARC'
+        when portfolio like 'South Indian Bank' then 'South Indian Bank'
+        when portfolio like 'City_Union' then 'CUB' 
+        when portfolio like 'City_Union' then 'CUB' 
+        when portfolio like 'Indian' then 'Indian Bank' 
+        else portfolio end as client
     from borrower
 ),
 
 portfolio_type as (
 
     select *,
-        case when Portfolio like 'Arcil'then 'ARC' 
-        when Portfolio like 'Areion' then 'ARC'
-        when Portfolio like 'Omkara' then 'ARC'
-        when Portfolio like 'Bank Of India' then 'Public Sector Bank'
-        when Portfolio like 'Bank Of Maharashtra' then 'Public Sector Bank' 
-        when Portfolio like 'City Union Bank' then 'Private Bank' 
-        when Portfolio like 'FEDBank Financial Services Limited' then 'NBFC' 
-        when Portfolio like 'Flexi Loan & Loan Singh' then 'NBFC' 
-        when Portfolio like 'IDBI Bank' then 'Private Bank' 
-        when Portfolio like 'Indian Bank' then 'Public Sector Bank' 
-        when Portfolio like 'Karur Vysya Bank' then 'Private Bank'
-        when Portfolio like 'Karnataka Bank' then 'Private Bank'
-        when Portfolio like 'Loan Tap' then 'NBFC'
-        when Portfolio like 'MPOWER' then 'NBFC'
-        when Portfolio like 'Magma' then 'NBFC'
-        when Portfolio like 'Punjab National Bank' then 'Public Sector Bank'
-        when Portfolio like 'South Indian Bank' then 'Private Bank'
-        when Portfolio like 'Canara Bank' then 'Private Bank'
-        when Portfolio like 'UCO Bank' then 'Public Sector Bank'
-        when Portfolio like 'Indian Overseas Bank' then 'Public Sector Bank'
-        when Portfolio like 'Union Bank Of India' then 'Public Sector Bank'
-        when Portfolio like 'Karur Vysysa' then 'Private Bank'
+        case when portfolio like 'Arcil'then 'ARC' 
+        when portfolio like 'Areion' then 'ARC'
+        when portfolio like 'Omkara' then 'ARC'
+        when portfolio like 'Bank Of India' then 'Public Sector Bank'
+        when portfolio like 'Bank Of Maharashtra' then 'Public Sector Bank' 
+        when portfolio like 'City Union Bank' then 'Private Bank' 
+        when portfolio like 'FEDBank Financial Services Limited' then 'NBFC' 
+        when portfolio like 'Flexi Loan & Loan Singh' then 'NBFC' 
+        when portfolio like 'IDBI Bank' then 'Private Bank' 
+        when portfolio like 'Indian Bank' then 'Public Sector Bank' 
+        when portfolio like 'Karur Vysya Bank' then 'Private Bank'
+        when portfolio like 'Karnataka Bank' then 'Private Bank'
+        when portfolio like 'Loan Tap' then 'NBFC'
+        when portfolio like 'MPOWER' then 'NBFC'
+        when portfolio like 'Magma' then 'NBFC'
+        when portfolio like 'Punjab National Bank' then 'Public Sector Bank'
+        when portfolio like 'South Indian Bank' then 'Private Bank'
+        when portfolio like 'Canara Bank' then 'Private Bank'
+        when portfolio like 'UCO Bank' then 'Public Sector Bank'
+        when portfolio like 'Indian Overseas Bank' then 'Public Sector Bank'
+        when portfolio like 'Union Bank Of India' then 'Public Sector Bank'
+        when portfolio like 'Karur Vysysa' then 'Private Bank'
         else 'Others' end as portfolio_type
 
     from client
