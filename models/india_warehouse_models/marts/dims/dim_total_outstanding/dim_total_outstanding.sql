5789f3ae625ddbd161931e5e43c294ebb605e951
vuli@bulaoutsourcing.com <vuli@bulaoutsourcing.com>
2022-05-18 22:11:34 +0000

    Create Dim TOS

---
+++ models/india_warehouse_models/marts/dims/dim_total_outstanding/dim_total_outstanding.sql
@@ -0,0 +1,22 @@
+{{config(
+    materialized ="table"
+)}}
+
+with total_outstanding as(
+
+    select * from {{ ref('stg_total_outstanding')}}
+),
+
+most_recent as (
+
+    select *,
+        closing_balance as total_outstanding -- new column
+    from total_outstanding as tos_1
+    where calculated_date = 
+
+        (	select max(calculated_date) as calculated_date                                                -- selecting the most recent pinstid
+            from total_outstanding as tos_2 where tos_1.pinstid = tos_2.pinstid 
+        )
+)
+
+Select * from most_recent
